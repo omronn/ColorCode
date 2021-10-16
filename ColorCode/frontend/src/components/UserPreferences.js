16a3b7cd0fa5a06a1493ea10de681a7b187e5ee7
Nicholas Omron <omronn@umich.edu>
2021-10-16 17:49:37 -0400

    set up bottons

--- ColorCode/frontend/src/components/UserPreferences.js
+++ ColorCode/frontend/src/components/UserPreferences.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
-import ToggleButton from "react-bootstrap/ToggleButton";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Switch from "react-switch";
+import Row from "react-bootstrap/Row";
 
-function UserPreferences(props) {
+function UserPreferences() {
     const [lightDark, setLightDark] = useState(false);
     const [neonPastel, setNeonPastel] = useState(false);
     const [oneManyHues, setOneManyHues] = useState(false);
@@ -13,51 +14,68 @@ function UserPreferences(props) {
     const [numColors, setNumColors] = useState(1);
     const [mainColor, setMainColor] = useState(0);
 
-    const requestOptions = {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-    };
+    const FetchPreferences = () => {
+        // Get preferences (new or old) and save the state
+        const requestOptions = {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        };
 
-    fetch("/api/UserPreferences", requestOptions).then(
-        (response) => {
-            if (!response.ok) throw Error(response.statusText);
-            return response.json();
-        }
-    ).then(
-        (data) => {
-            setLightDark(data.light_dark);
-            setNeonPastel(data.neon_pastel);
-            setOneManyHues(data.one_many_hues);
-            setBoldSubtle(data.bold_subtle);
-            setNumColors(data.num_colors);
-            setMainColor(data.main_color);
-        }
-    ).catch((error) => console.log(error));
+        fetch("/api/UserPreferences", requestOptions).then(
+            (response) => {
+                if (!response.ok) throw Error(response.statusText);
+                return response.json();
+            }
+        ).then(
+            (data) => {
+                setLightDark(data.light_dark);
+                setNeonPastel(data.neon_pastel);
+                setOneManyHues(data.one_many_hues);
+                setBoldSubtle(data.bold_subtle);
+                setNumColors(data.num_colors);
+                setMainColor(data.main_color);
+            }
+        ).catch((error) => console.log("I fucked up"));
+    }
+    
+    // Functions for subcomponents
+    const Preferences = () => {
+        return (
+            <Container fluid className="p-5 my-5 bg-secondary align-self-center">
+                <Form>
+                    <Row>
+                        Light <Switch onChange={() => { setLightDark(!lightDark); } } checked={lightDark}/> Dark
+                    </Row>
+                    <Row>
+                        Neon <Switch onChange={() => { setNeonPastel(!neonPastel); } } checked={neonPastel}/> Pastel
+                    </Row>
+                    <Row>
+                        Bold <Switch onChange={() => { setBoldSubtle(!boldSubtle); } } checked={boldSubtle}/> Subtle
+                    </Row>
+                    <Row>
+                        One Hue <Switch onChange={() => { setOneManyHues(!oneManyHues); } } checked={oneManyHues}/> Many Hues
+                    </Row>
+                </Form>
+            </Container>
+        );
+    }
 
+    const GenerateButton = () => {
+        return (
+            <Button onClick={ GenerateButton } variant="success">Generate!</Button>
+        );
+    }
+
+    // Fetches preferences once, upon page load
+    useEffect(FetchPreferences, []);
+
+    // TODO Not sure why oneManyHues is getting sent as true.  
     return (
         <Container fluid className="vh-100 p-5 bg-dark text-white text-center">
             <h1>I Want...</h1>
-            <Preferences lightDark={lightDark} neonPastel={neonPastel}
-                oneManyHues={oneManyHues} boldSubtle={boldSubtle}
-                numColors={numColors} mainColor={mainColor}/>
+            <Preferences />
+            <GenerateButton />
         </Container>
-    );
-}
-
-function Preferences(props) {
-    return (
-        <> 
-            <Container fluid className="p-5 my-5 bg-secondary align-self-center">
-                <h1>Buttons Go Here</h1>
-            </Container>
-            <Button variant="success">Generate!</Button>{''}    
-        </>
-    );
-}
-
-function GenerateButton(props) {
-    return (
-        <div></div>
     );
 }
 
