721630a3e81c7525ea8fe704b67bfdd1dae8bef2
Nicholas Omron <omronn@umich.edu>
2021-10-26 16:42:49 -0400

    palette display

--- ColorCode/frontend/src/components/Palette.js
+++ ColorCode/frontend/src/components/Palette.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+//import '../static/index.css';
 
 function Palette() {
     // TODO Declare Palette vars and setters with useState
     const [baseColor, setBaseColor] = useState("000000");
-    const [colorJsonList, setJsonList] = useState([])
+    const [colorJsonList, setJsonList] = useState([]);
 
     const FetchPalette = () => {
         // Get preferences (new or old) and save the state
@@ -27,6 +30,9 @@ function Palette() {
                 setBaseColor(data.base_color);
                 // Convert to usable list
                 setJsonList(JSON.parse(data.palette_list));
+                //assign colors to css classes
+
+                
             }
         ).catch((error) => {
             console.log("error");
@@ -48,22 +54,69 @@ function Palette() {
         );
     }
 
+    const FakeSite = () => {
+        // TODO: edit this html based on the number of colors included.
+
+        return (
+            <Container className="palette_box">
+                
+                <Row className="fake-header">
+                    <Col sm>
+                        Title
+                    </Col>
+                    <Col>
+                        Nav buttons
+                    </Col>
+                </Row>
+
+                <Row className="fake-body">
+                    <Col></Col>
+                    <Col lg>
+                        <div class="fake-main-window">
+                            <div class="fake-alert">Alert! Please note that...</div>
+                            <div>This is the text in a primary window! Information goes here.</div>
+                            <div class="fake-button">Click here!</div>
+                        </div>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row class="fake-body">
+                    <Col></Col>
+                    <Col lg>
+                        <div class="fake-secondary-window">This is a secondary window with additional info.</div>
+                    </Col>
+                    <Col></Col>
+                </Row>
+
+                <Row class="fake-header">
+                    <Col sm>
+                    Footer. Socmed links. Copyright information
+                    </Col>
+                </Row>
+                
+            
+            </Container>
+        );
+    }
+
+    // ACTUALLY RUNS WHEN COMPONENT RENDERS
+    
     // Uncomment once Palette backend works 
     useEffect(FetchPalette, []);  // KEEPME, WILL USE ONCE FETCHPALETTE DATA DONE
 
-    // Makes colored container for each color. Temp way to display colors
-    const containers = []
+    const color_list = []
     for (const [index, value] of colorJsonList.entries()) {
-        containers.push(<Container fluid className="p-3 my-3 align-self-center" style={{ backgroundColor: '#' + value }}></Container>)
+        color_list.push(
+            <Container className="md p-1 my-1 text-white text-center align-self-center" style={{ backgroundColor: '#' + value }}>Color: #{value}</Container>
+        );
     }
 
     return (
         <Container fluid className="vh-100 p-5 bg-dark text-white text-center">
+            <h1> Example Color Usage:</h1>
+            <FakeSite />
             <h1>THE PALETTE:</h1>
-            <div> Main Color: </div>
-            <Container fluid className="p-3 my-3 align-self-center" style={{ backgroundColor: '#' + baseColor }}></Container>
-            <div> Generated Colors: </div>
-            { containers }
+            {color_list}
             <BackButton />
         </Container>
     );
--- ColorCode/frontend/src/index.js
+++ ColorCode/frontend/src/index.js
@@ -2,5 +2,6 @@ import App from './components/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import '../static/csindex.css';
 
 ReactDOM.render(<App />, document.getElementById("app"));
