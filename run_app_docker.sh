dcc6845d7b1f5618a1f58af56fb46d14c4ebf5d2
Doug <djmcdo@umich.edu>
2021-11-11 16:10:52 -0500

    added step by step instructions

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# TO RUN COLORCODE on Windows:
+#### SET-UP TO RUN COLORCODE on Windows: ####
 # Prereq: Have docker-compose installed
 # 1) Open windows powershell
 # 2) cd to the directory that contains this file
 # 3) run "dos2unix run_app_docker.sh" to ensure that this file works properly
 # 3a) TROUBLESHOOT: if dos2unix was not recognized, run "bash" and then continue from step 3
 # 4) run "./run_app_docker.sh"
-# 5) visit localhost:8000, enjoy!
+# 5) visit localhost:8000. See 'USE' instructions below for a step-by-step guide
 # 6) Once done, run "docker-compose down" in the directory that contains this file
 # 7) All Done!
+
+#### USE: ####
+# 1) Select desired settings on the preferences page
+# 2) Click “generate” to navigate to the generated palette page
+# 3) Explore mockup interfaces using the numbered buttons
+# 4) If color palette is unsatisfactory, click “Back” and repeat steps 1-3 to adjust settings
+# 5) Once satisfied, click “Export” to download the color palette as a .txt file
+
 
 # build image
 docker-compose build 
