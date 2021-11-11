85081d2d37eab67b80c0513116b1102cbc6bffa8
Doug <djmcdo@umich.edu>
2021-11-11 15:56:58 -0500

    edited instructions slightly

--- run_app_docker.sh
+++ run_app_docker.sh
@@ -5,7 +5,8 @@
 # 1) Open windows powershell
 # 2) cd to the directory that contains this file
 # 3) run "dos2unix run_app_docker.sh" to ensure that this file works properly
-# 4) run "./run_app_docker.sh" (may be "bash ./run_app_docker.sh" on some systems)
+# 3a) TROUBLESHOOT: if dos2unix was not recognized, run "bash" and then continue from step 3
+# 4) run "./run_app_docker.sh"
 # 5) visit localhost:8000, enjoy!
 # 6) Once done, run "docker-compose down" in the directory that contains this file
 # 7) All Done!
